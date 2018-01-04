657a812f3a98f34007a34e06fb0afcf00919ceda
laxmibarpete <laxmibarpete1994@gmail.com>
2018-01-05 01:18:24 +0530

    add upload path

--- mediaide/models.py
+++ mediaide/models.py
@@ -62,7 +62,7 @@ def user_directory_path(instance, filename):
 class UserDocuments(models.Model):
     user = models.ForeignKey(CustomUser,on_delete=models.CASCADE, related_name='document')
     description = models.CharField(max_length=255, blank=True)
-    document = models.FileField(upload_to='user_directory_path')
+    document = models.FileField(upload_to=user_directory_path)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
 
