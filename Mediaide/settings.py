b69e5c6ad3323bf904d61bad5803a5060c881c4a
laxmibarpete <34623564+laxmibarpete@users.noreply.github.com>
2018-01-18 23:55:42 +0530

    Update settings.py

--- Mediaide/settings.py
+++ Mediaide/settings.py
@@ -30,8 +30,15 @@ DEBUG = False
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT = os.path.join(BASE_DIR, ‘staticfiles’)
 ALLOWED_HOSTS = ['*']
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+
 
 JWT_ALGORITHM = 'HS256'
 
@@ -191,6 +198,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Email setting
 EMAIL_USE_TLS = True
